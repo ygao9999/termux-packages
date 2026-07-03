4f8dde3c2e7c8d4ec4b7b74afc25710828b2f80e
ygao9999 <118158518@qq.com>
2026-07-03 21:10:27 +0800

    Refactor Nano package dependencies
    Updated package dependencies for Nano editor. Removed runtime dependency on ncurses and added build dependency on ncurses-static.

--- packages/nano/build.sh
+++ packages/nano/build.sh
@@ -6,7 +6,10 @@ TERMUX_PKG_VERSION="9.1"
 TERMUX_PKG_SRCURL=https://nano-editor.org/dist/latest/nano-$TERMUX_PKG_VERSION.tar.xz
 TERMUX_PKG_SHA256=5f47764274cb7532349ce0aa20ec10f1e8e851a6e9fa3eb66812c43d196db042
 TERMUX_PKG_AUTO_UPDATE=true
-TERMUX_PKG_DEPENDS="libandroid-support, ncurses"
+# 运行时不再依赖 ncurses 的 .so（已静态链接进二进制），只留 libandroid-support
+TERMUX_PKG_DEPENDS="libandroid-support"
+# 构建时需要 ncurses 的静态库（.a）
+TERMUX_PKG_BUILD_DEPENDS="ncurses-static"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 ac_cv_header_glob_h=no
 ac_cv_header_pwd_h=no
@@ -15,6 +18,9 @@ gl_cv_func_strcasecmp_works=yes
 --enable-utf8
 --with-wordbounds
 "
+# 强制把 ncursesw 静态链接进二进制，libc/libm 等系统库仍走动态链接
+TERMUX_PKG_EXTRA_LDFLAGS+=" -l:libncursesw.a"
+
 TERMUX_PKG_CONFFILES="etc/nanorc"
 TERMUX_PKG_RM_AFTER_INSTALL="bin/rnano share/man/man1/rnano.1 share/nano/man-html"
 
